7a2443c55081acadfc2918bf45e71d4535b386b6
Jesse Gallagher <jesse@iknowsomeguys.com>
2017-10-10 08:35:11 -0400

    Initial move from darwino-platform and org.darwino rename

---
+++ p2sitexml-maven-plugin/src/main/java/org/darwino/maven/p2sitexml/GenerateSiteXmlMojo.java
@@ -0,0 +1,112 @@
+package org.darwino.maven.p2sitexml;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.ibm.commons.util.StringUtil;
+import com.ibm.commons.util.io.StreamUtil;
+import com.ibm.commons.xml.DOMUtil;
+import com.ibm.commons.xml.XMLException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Goal which generates a site.xml file from a features directory.
+ */
+@Mojo(name="generate-site-xml", defaultPhase=LifecyclePhase.PREPARE_PACKAGE)
+public class GenerateSiteXmlMojo extends AbstractMojo {
+	private static final Pattern FEATURE_FILENAME_PATTERN = Pattern.compile("^([^_]+)_(.*)\\.jar$"); //$NON-NLS-1$
+	
+	/**
+	 * Location of the p2 repository.
+	 */
+	@Parameter(defaultValue="${project.build.directory}/repository", property="p2Directory", required=false)
+	private File p2Directory;
+	/**
+	 * The category name to use for the features in the site.
+	 */
+	@Parameter(defaultValue="", property="category", required=false)
+	private String category;
+
+	@SuppressWarnings("resource")
+	@Override
+	public void execute() throws MojoExecutionException {
+		File f = p2Directory;
+
+		if (!f.exists() || !f.isDirectory()) {
+			throw new MojoExecutionException("Repository directory does not exist.");
+		} else {
+			File features = new File(f, "features"); //$NON-NLS-1$
+			if(!features.exists() || !features.isDirectory()) {
+				throw new MojoExecutionException("Unable to find features directory: " + features.getAbsolutePath());
+			}
+			
+			try {
+				Document doc = DOMUtil.createDocument();
+				Element root = DOMUtil.createElement(doc, "site"); //$NON-NLS-1$
+				
+				// Create the category entry if applicable
+				String category = this.category;
+				if(StringUtil.isNotEmpty(category)) {
+					Element categoryDef = DOMUtil.createElement(doc, root, "category-def"); //$NON-NLS-1$
+					categoryDef.setAttribute("name", category); //$NON-NLS-1$
+					categoryDef.setAttribute("label", category); //$NON-NLS-1$
+				}
+				
+				String[] featureFiles = features.list(new FilenameFilter() {
+					@Override public boolean accept(File parent, String fileName) {
+						return StringUtil.toString(fileName).toLowerCase().endsWith(".jar"); //$NON-NLS-1$
+					}
+				});
+				
+				for(String featureFilename : featureFiles) {
+					Matcher matcher = FEATURE_FILENAME_PATTERN.matcher(featureFilename);
+					if(!matcher.matches()) {
+						throw new IllegalStateException("Could not match filename pattern to " + featureFilename);
+					}
+					getLog().debug("Filename matcher groups: " + matcher.groupCount());
+					String featureName = matcher.group(1);
+					String version = matcher.group(2);
+					
+					Element featureElement = DOMUtil.createElement(doc, root, "feature"); //$NON-NLS-1$
+					String url = "features/" + featureFilename; //$NON-NLS-1$
+					featureElement.setAttribute("url", url); //$NON-NLS-1$
+					featureElement.setAttribute("id", featureName); //$NON-NLS-1$
+					featureElement.setAttribute("version", version); //$NON-NLS-1$
+					
+					if(StringUtil.isNotEmpty(category)) {
+						Element categoryElement = DOMUtil.createElement(doc, featureElement, "category"); //$NON-NLS-1$
+						categoryElement.setAttribute("name", category); //$NON-NLS-1$
+					}
+				}
+				
+				String xml = DOMUtil.getXMLString(doc);
+				File output = new File(f, "site.xml"); //$NON-NLS-1$
+				FileWriter w = null;
+				try {
+					w = new FileWriter(output);
+					w.write(xml);
+				} catch (IOException e) {
+					throw new MojoExecutionException("Error writing site.xml file", e);
+				} finally {
+					StreamUtil.close(w);
+				}
+				
+				getLog().info(StringUtil.format("Wrote site.xml contents to {0}", output.getAbsolutePath()));
+			} catch(XMLException e) {
+				throw new MojoExecutionException("Exception while building site.xml document", e);
+			}
+		}
+	}
+}
